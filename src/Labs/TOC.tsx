a663fec7c3d775c229eefaec6790009d48491c78
Excalibur0331 <eli1838459978@gmail.com>
2024-09-18 16:16:01 -0700

    add personal info

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function TOC() {
   return (
     <ul>
-      <li><Link to="/Labs">Labs</Link></li>
+      <li><a id = "wd_github" href="https://github.com/Liheng-Yi/kanbas-react-web-app" target="_blank">Liheng Yi's github</a></li>
       <li><Link to="/Labs/Lab1">Lab 1</Link></li>
       <li><Link to="/Labs/Lab2">Lab 2</Link></li>
       <li><Link to="/Labs/Lab3">Lab 3</Link></li>
