e9d99c4350d91899bcdc7397f5bf16cf50e72807
Excalibur0331 <eli1838459978@gmail.com>
2024-09-30 22:47:41 -0700

    add mutiple screens

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,53 +1,71 @@
+import ModulesControls from "./ModulesControls";
+import { BsGripVertical } from "react-icons/bs";
+import LessonControlButtons from "./LessonControlButtons";
 export default function Modules() {
-    const handleCollapseAll = () => {
+  const handleCollapseAll = () => {
     console.log("Collapse All clicked");
-    };
+  };
 
-    const handleViewProgress = () => {
-        console.log("View Progress clicked");
-    };
+  const handleViewProgress = () => {
+    console.log("View Progress clicked");
+  };
 
-    const handlePublishAll = () => {
-        console.log("Publish All clicked");
-    };
+  const handlePublishAll = () => {
+    console.log("Publish All clicked");
+  };
 
-    const handleAddModule = () => {
-        console.log("+ Module clicked");
-    };
+  const handleAddModule = () => {
+    console.log("+ Module clicked");
+  };
 
+  return (
+    <div>
+      <div className="module-buttons">
+        <button onClick={handleCollapseAll}>Collapse All</button>
+        <button onClick={handleViewProgress}>View Progress</button>
+        <button onClick={handlePublishAll}>Publish All</button>
+        <button onClick={handleAddModule}>+ Module</button>
+      </div>
 
-
-    return (
-        <div>
-            <div className="module-buttons">
-                <button onClick={handleCollapseAll}>Collapse All</button>
-                <button onClick={handleViewProgress}>View Progress</button>
-                <button onClick={handlePublishAll}>Publish All</button>
-                <button onClick={handleAddModule}>+ Module</button>
+      <div>
+        <ModulesControls /><br /><br /><br /><br />
+        <ul id="wd-modules" className="list-group rounded-0">
+          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary">
+              <BsGripVertical className="me-2 fs-3" />
+              Week 1
+              <LessonControlButtons />
             </div>
-
-            <ul id="wd-modules">
-                <li className="wd-module">
-                    <div className="wd-title">Week 1</div>
-                    <ul className="wd-lessons">
-                        <li className="wd-lesson">
-                            <span className="wd-title">LEARNING OBJECTIVES</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Introduction to the course</li>
-                                <li className="wd-content-item">Learn what is Web Development</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li className="wd-module">
-                    <div className="wd-title">Week 2</div>
-                    <ul className="wd-lessons">
-                        <li className="wd-lesson">
-                            <span className="wd-title">LEARNING OBJECTIVES</span>
-                        </li>
-                    </ul>
-                </li>
+            <ul className="wd-lessons list-group rounded-0">
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                <BsGripVertical className="me-2 fs-3" />
+                LEARNING OBJECTIVES
+              </li>
+              <LessonControlButtons />
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                <BsGripVertical className="me-2 fs-3" />
+                Introduction to the course
+              </li>
+              <LessonControlButtons />
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                Learn what is Web Development
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1 </li>
+              <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 2 </li>
             </ul>
-        </div>
-    );
+          </li>
+          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+            <ul className="wd-lessons list-group rounded-0">
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                LEARNING OBJECTIVES
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1 </li>
+              <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 2 </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -6,6 +6,7 @@ import Courses from "./Courses";
 import { Link } from "react-router-dom";
 import TOC from "../Labs/TOC";
 import { useNavigate } from "react-router-dom";
+import "./styles.css";
 
 
 export default function Kanbas() {
@@ -19,24 +20,20 @@ export default function Kanbas() {
     <div id="wd-kanbas">
 
       <button onClick={() => goToLab1()}>Go back to Landing page</button>
-      <table>
-        <tr>
-          <td valign="top">
+
             <KanbasNavigation />
-          </td>
-          <td valign="top">
-            
-            <Routes>
-              <Route path="/" element={<Navigate to="/Kanbas/Account" />} />
-              <Route path="/Account/*" element={<Account />} />
-              <Route path="/Dashboard" element={<Dashboard />} />
-              <Route path="/Courses/:cid/*" element={<Courses />} />
-              <Route path="/Calendar" element={<h1>Calendar</h1>} />
-              <Route path="/Inbox" element={<h1>Inbox</h1>} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
+
+            <div className="wd-main-content-offset p-3">
+              <Routes>
+                <Route path="/" element={<Navigate to="/Kanbas/Dashboard" />} />
+                <Route path="/Account/*" element={<Account />} />
+                <Route path="/Dashboard" element={<Dashboard />} />
+                <Route path="/Courses/:cid/*" element={<Courses />} />
+                <Route path="/Calendar" element={<h1>Calendar</h1>} />
+                <Route path="/Inbox" element={<h1>Inbox</h1>} />
+              </Routes>
+            </div>
+
     </div>
   );
 }
